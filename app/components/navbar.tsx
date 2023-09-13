16fa80ad8ebab1a920c5bf8df419b09201213052
Toyehamzat <anibiseun@gmail.com>
2023-09-13 11:56:35 +0100

    complete navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link"; 
+import Link from "next/link";
+import { FaYoutube, FaWhatsapp, FaTwitch, FaLaptop } from "react-icons/fa";
 export default function Navbar() {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
@@ -11,6 +12,20 @@ export default function Navbar() {
             Toye Hamzat
           </Link>
         </h1>
+        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-3xl lg:text-4xl">
+          <Link href="" className="text-white/90 no-underline hover:text-white">
+            <FaYoutube />
+          </Link>
+          <Link href="" className="text-white/90 no-underline hover:text-white">
+            <FaWhatsapp />
+          </Link>
+          <Link href="" className="text-white/90 no-underline hover:text-white">
+            <FaTwitch />
+          </Link>
+          <Link href="" className="text-white/90 no-underline hover:text-white">
+            <FaLaptop />
+          </Link>
+        </div>
       </div>
     </nav>
   );
