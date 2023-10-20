247d152c869b9f47618a33ac6cd33d9b08cb3453
Toyehamzat <anibiseun@gmail.com>
2023-10-20 09:44:12 +0100

    blog completed

--- app/Posts/[PostsId]/page.tsx
+++ app/Posts/[PostsId]/page.tsx
@@ -1,22 +1,24 @@
-import { GetSortedPostsData } from "@/lib/post";
+import GetFormattedDate from "@/lib/getFormattedDtate";
+import { GetSortedPostsData, GetPostData } from "@/lib/post";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { title } from "process";
 
-export  function GenerateMetaData({ params }: { params: { PostsId: string } }) {
+export function GenerateMetaData({ params }: { params: { PostsId: string } }) {
   const posts = GetSortedPostsData();
   const { PostsId } = params;
 
-  const post= posts.find(post => post.id === PostsId)
+  const post = posts.find((post) => post.id === PostsId);
 
-if (!post) {
-    return{
-        title: 'Post not found'
-    }
-}
-
-  return{
-    title: post.title, 
+  if (!post) {
+    return {
+      title: "Post not found",
+    };
   }
+
+  return {
+    title: post.title,
+  };
 }
 
 export default async function Post({
@@ -30,9 +32,18 @@ export default async function Post({
   if (!posts.find((post) => post.id === PostsId)) {
     return notFound();
   }
+  const { title, date, contentHtml } = await GetPostData(PostsId);
+  const pubDate = GetFormattedDate(date);
   return (
-    <div>
-      <p>p</p>
-    </div>
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-6 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/">back to Home</Link>
+        </p>
+      </article>
+    </main>
   );
 }
--- app/components/PostItem.tsx
+++ app/components/PostItem.tsx
@@ -8,14 +8,16 @@ export default function PostItem({ post }: Props) {
   const { id, title, date } = post;
   const newDate = GetFormattedDate(date);
   return (
-    <li className="text-left mt-4 text-2xl dark:text-white/90" key={id}>
-      <Link
-        className="underline hover:text-black/70 dark:hover:text-slate-300"
-        href={`/Post/${id}`}
-      >
-        {title}
-      </Link>
-      <p className="text-sm mt-1">{newDate}</p>
-    </li>
+    <div>
+      <li className="text-left mt-4 text-2xl dark:text-white/90" key={id}>
+        <Link
+          className="underline hover:text-black/70 dark:hover:text-slate-300"
+          href={`/Posts/${id}`}
+        >
+          {title}
+        </Link>
+        <p className="text-sm mt-1">{newDate}</p>
+      </li>
+    </div>
   );
 }
